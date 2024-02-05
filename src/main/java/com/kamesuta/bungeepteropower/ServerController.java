735c60cbfe94b888e789976d1fd26c46b19c5e7d
Lefraudeur <karlbouisseren@gmail.com>
2024-02-05 19:48:53 +0100

    Improved how startup join timeout works

--- src/main/java/com/kamesuta/bungeepteropower/ServerController.java
+++ src/main/java/com/kamesuta/bungeepteropower/ServerController.java
@@ -3,12 +3,10 @@ package com.kamesuta.bungeepteropower;
 import com.kamesuta.bungeepteropower.api.PowerSignal;
 import net.md_5.bungee.api.Callback;
 import net.md_5.bungee.api.CommandSender;
-import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
-import java.time.Instant;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
@@ -77,35 +75,21 @@ public class ServerController {
      * @return A future that completes when the server is started
      */
     private static CompletableFuture<Void> onceStarted(ServerInfo serverInfo) {
-        CompletableFuture<Void> future = new CompletableFuture<Void>();
-
-        // The timestamp when the server is expected to be started within
-        Instant timeout = Instant.now().plusSeconds(plugin.config.startupJoinTimeout);
-
-        // Ping the server and check if it is started
-        Callback<ServerPing> callback = new Callback<>() {
+        CompletableFuture<Void> future = new CompletableFuture<Void>().orTimeout(plugin.config.startupJoinTimeout, TimeUnit.SECONDS);
+        Callback<ServerPing> callback = new Callback<ServerPing>() {
             @Override
             public void done(ServerPing serverPing, Throwable throwable) {
-                // If the server is started, complete the future
+                if (future.isDone())
+                    return;
                 if (throwable == null && serverPing != null) {
                     future.complete(null);
                     return;
                 }
-                // Not started yet, retry after a while
-                if (Instant.now().isBefore(timeout)) {
-                    ProxyServer.getInstance().getScheduler()
-                            .schedule(plugin, () -> serverInfo.ping(this), plugin.config.pingInterval, TimeUnit.SECONDS);
-                    return;
-                }
-
-                // If the server is not started within the timeout, complete the future exceptionally
-                future.completeExceptionally(new RuntimeException("Server did not start in autoJoinTimeout"));
+                plugin.getProxy().getScheduler().schedule(plugin, ()-> serverInfo.ping(this), plugin.config.pingInterval, TimeUnit.SECONDS);
             }
         };
         serverInfo.ping(callback);
-
         return future;
-
     }
 
 }
